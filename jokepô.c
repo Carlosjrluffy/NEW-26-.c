9bcb8c30890268fb12374ab5bd43d382c9447c2f
Carlosjrluffy <carlosjr_@outlook.com>
2026-05-25 15:25:17 -0300

    Refactor Jogo de Jokenpô logic and user input

--- jokepô.c
+++ jokepô.c
@@ -4,47 +4,64 @@
 
 int main(){
 
-    int opcao;
-    int numerosecreto, palpite;
+  int escolhajogador, escolhacomputador;
+  srand(time(0));
 
-    printf("*Menu Principal*\n");
-    printf("1. Iniciar o game\n");
-    printf("2.Ver Regras\n");
-    printf("3.Sair\n");
-    printf("Escolhar uma opção: ");
-    scanf("%d", &opcao);
+    printf("*JOGO DE JOKENPÔ*\n");
+    printf("|Escolha uma opção: \n");
+    printf("1. Pedra\n");
+    printf("2.Papel\n");
+    printf("3.Tesoura\n");
+    printf("Escolha: \n");
+    scanf("%d", &escolhajogador);
 
-
-    switch (opcao)
-    {
-     case 1: 
-        srand(time(0));
-        numerosecreto = rand() %10;
-        printf("Digite um numero de 0 a 9: ");
-        scanf("%d", &palpite);
-        if (numerosecreto == palpite)
-        {
-            printf("Você acertou.\n");
-            printf("Numero secreto %d", numerosecreto);
-        }
-        else{
-            printf("você errou.\n");
-            printf("Numero secreto %d", numerosecreto);
-        }       
-        break;
-        case 2:
-            printf("A explicação das regras.\n");
-        break;
-        case 3:
-            printf("Saindo do game.\n");
-        break;
-    
-        default:
-        printf("Opção invalida!\n");
-        break;
+    // Validação da escolha do jogador antes de continuar
+    if (escolhajogador < 1 || escolhajogador > 3) {
+        printf("Escolha inválida. O programa será encerrado.\n");
+        return 0; // Encerra o programa aqui se a entrada for inválida
     }
-   
+    escolhacomputador = rand() %3 +1;
 
+    switch (escolhajogador)
+    {
+    case 1:
+        printf("Jogador: Pedra - ");
+        break;
+    case 2:
+        printf("Jogador: Papel - ");
+        break;
+    case 3:
+        printf("Jogador: Tesoura - ");
+        break;   
+    }
 
+     switch (escolhacomputador)
+    {
+    case 1:
+        printf("computador: Pedra ");
+        break;
+    case 2:
+        printf("computador: Papel ");
+        break;
+    case 3:
+        printf("computador: Tesoura ");
+        break;   
+    }
 
+    // Verifica o resultado do jogo 
+    if(escolhajogador == escolhacomputador)
+    {
+        printf("### Você empatou! ###\n");
+
+    } else if ((escolhajogador == 1) && (escolhacomputador == 3) || (escolhajogador == 2) && (escolhacomputador == 1) || (escolhajogador == 3) && (escolhacomputador == 2))
+    {
+        printf("### Você Ganhou! ###\n");
+        
+    } else {
+        printf("### Você perdeu! ###\n");
+    }
+        
+    
+    
+    return 0;
 }
